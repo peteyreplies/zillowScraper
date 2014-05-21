1b289e673b088d7062fd6a4bad4a07fc1aa426fd
peteyMIT <petey@mit.edu>
2014-05-21 17:27:58 -0400

    works for reg results, switching to deep

--- zillowScraper.py
+++ zillowScraper.py
@@ -8,15 +8,15 @@ import urllib2
 
 
 ##set variables
-ZKEY = 'X1-ZWz1dtw75sqd57_39zrb'
+zpid = 'X1-ZWz1dtw75sqd57_39zrb'
 address = '69+Summer+St+Apt+2'
 citystate = 'Watertown%2C+MA'
 baseGetResults = 'http://www.zillow.com/webservice/GetSearchResults.htm'
+baseGetDeep = 'http://www.zillow.com/webservice/GetDeepSearchResults.htm'
 
 ##get & parse results
 resultURL = baseGetResults + '?zws-id=' + ZKEY + '&address=' + address + '&citystatezip=' + citystate
-resultParsed = xmltodict.parse(result)
+resultParsed = xmltodict.parse(urllib2.urlopen(resultURL).read())
+u_zpid = data['SearchResults:searchresults']['response']['results']['result']['zpid']
+zpid = str(u_zpid)
 
-print resultParsed
-
-data=xmltodict.parse(urllib2.urlopen(url).read())
